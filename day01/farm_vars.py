9140624d4296314532e261707f8044a4da32b801
Seri Pramod Reddy <seripramodreddy619@gmail.com>
2026-06-16 19:00:29 +0530

    Day 2: fix print style - no space before parens

--- day01/farm_vars.py
+++ day01/farm_vars.py
@@ -3,4 +3,10 @@ farm_location = "Agnoor Village"
 total_acres = 14
 pigeonpea_acres = 7
 maize_acres = 7
-cost_per_acre = 1000000 / total_acres
+total_investment_inr = 1000000
+cost_per_acre = total_investment_inr / total_acres
+print(f"farm in {farm_location} has total area of {total_acres} acres")
+print(f"pigeon pea is planned to sow in {pigeonpea_acres} acres")
+print(f"maize is planned to sow in {maize_acres} acres")
+print(f"cost per acre is {cost_per_acre} rupees")
+print(f"total investment is {total_investment_inr} rupees")
