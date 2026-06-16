bca8eff7f7f4234e1c60fafc5c068c2960db0d0f
Seri Pramod Reddy <seripramodreddy619@gmail.com>
2026-06-16 19:17:41 +0530

    Day 3: fix - use loop instead of manual print

---
+++ day01/farm_fields.py
@@ -0,0 +1,6 @@
+ #farm_vars.py | Seri Pramod Reddy | Day 3 | June 2026
+fields = ["North_field", "South_field", "East_field", "West_field"]
+fields_acres = [3,4,3,4]
+fields_crops = ["corn", "wheat", "soybeans", "barley"]
+for i in range(len(fields)):
+    print(f"{fields[i]} | {fields_acres[i]} acres | {fields_crops[i]}")
