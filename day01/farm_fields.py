30131f8d6bf6ff79a50f75ce21daf1f1dc19a069
Seri Pramod Reddy <seripramodreddy619@gmail.com>
2026-06-16 19:20:55 +0530

    Day 3: fix - use loop instead of manual print

--- day01/farm_fields.py
+++ day01/farm_fields.py
@@ -1,4 +1,4 @@
- #farm_vars.py | Seri Pramod Reddy | Day 3 | June 2026
+ #farm_fields.py | Seri Pramod Reddy | Day 3 | June 2026
 fields = ["North_field", "South_field", "East_field", "West_field"]
 fields_acres = [3,4,3,4]
 fields_crops = ["corn", "wheat", "soybeans", "barley"]
