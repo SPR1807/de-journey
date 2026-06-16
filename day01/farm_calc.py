19687e1d91b09759da0b9122feb6de9fd30f1c16
Seri Pramod Reddy <seripramodreddy619@gmail.com>
2026-06-16 18:43:32 +0530

    Day 1: add file header comment

---
+++ day01/farm_calc.py
@@ -0,0 +1,9 @@
+# farm_calc.py | Seri Pramod Reddy | Day 1 | June 2026
+# Details of the Farm
+print ("Farm is in Agnoor Village")
+print ("Total Area = 14 Acres")
+# Crops Planned to Sow
+print("Pigeon Pea in the First half")
+print("Maize in the second half")
+#Turnover expecting
+print("total Rupees = 1000000")
